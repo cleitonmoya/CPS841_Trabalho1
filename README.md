cd1b6973787a72622e3eb04534fedb7991b9b268
Cleiton Moya de Almeida <cleitonmoya@gmail.com>
2020-05-15 23:56:16 -0300

    Inlusão do arquivo de testestes da Wisard

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Olá mundo
+CPS841 - Redes Neurais Sem Peso (Weightless Neural Network)
+Professor: Priscila Machado Vieira Lima
+Student: Cleiton Moya de Almeida
+
+This work aims to apply the WiSARD model to the MNIST database.
+
---
+++ testeWisard.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Teste da biblioteca Wisard
+Aluno: Cleiton Moya de Almeida
+"""
+import wisardpkg as wp
+
+# load input data, just zeros and ones  
+X = [
+      [1,1,1,0,0,0,0,0],
+      [1,1,1,1,0,0,0,0],
+      [0,0,0,0,1,1,1,1],
+      [0,0,0,0,0,1,1,1]
+    ]
+
+# load label data, which must be a string array
+y = [
+      "cold",
+      "cold",
+      "hot",
+      "hot"
+    ]
+
+
+addressSize = 3     # number of addressing bits in the ram
+ignoreZero  = False # optional; causes the rams to ignore the address 0
+
+# False by default for performance reasons,
+# when True, WiSARD prints the progress of train() and classify()
+verbose = True
+
+wsd = wp.Wisard(addressSize, ignoreZero=ignoreZero, verbose=verbose)
+
+
+# train using the input data
+wsd.train(X,y)
+
+# classify some data
+out = wsd.classify(X)
+
+# the output of classify is a string list in the same sequence as the input
+for i,d in enumerate(X):
+    print(out[i],d)
