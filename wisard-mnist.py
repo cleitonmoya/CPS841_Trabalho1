d1a65d9f48f6659fa77066a8737849bd646e9a64
Cleiton Moya de Almeida <cleitonmoya@gmail.com>
2020-05-18 20:39:35 -0300

    n experimentos

--- wisard-mnist.py
+++ wisard-mnist.py
@@ -6,7 +6,6 @@ Aplicação do classificador WiSARD na base de dados MNIST
 Aluno: Cleiton Moya de Almeida
 
 A implementar:
-    - veriicar se dá para fixar o mapeamento
     - testar o melhor n_i entre {2, 4, 7, 8, 14, 16}, 28, 
     - criar uma função que, uma vez treinada a rede, retorna a classificaão
       de um exemplar aleatório, retornando a medida de similaridade de cada
@@ -15,6 +14,7 @@ A implementar:
 """
 
 import time
+import random
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -22,13 +22,48 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 from PIL import Image
 import wisardpkg as wp
 
+
+# ---------------------------- FUNÇÕES AUXILIARES ----------------------------
+
+
+def plotaExemplaresConfusao(X,Y,G,y,g):
+    # data-frame com todas as classificações
+    df = pd.DataFrame({'Y':Y,'G':G,'X':X})
+    
+    # classificações incorretas da classe 'c'
+    dfC = df.loc[(df['Y']==y) & (df['G']==g)]
+    
+    # sorteia 10 exemplares mal-classificados
+    n, _ = dfC.shape
+
+    if n == 0:
+        print("Não há confusão para o 'y' e 'g' informado")
+    else:
+        if n<10: 
+            k = n 
+        else: 
+            k = 10
+            
+        pos = random.sample(range(n),k)
+        dfR = dfC.iloc[pos]
+        R =  dfR['X'].to_numpy()
+    
+        # plota os exemplares mal-classificados
+        for k,arr in enumerate(R):
+            arr = np.array(arr)
+            plt.subplot(2,5,k+1)
+            plt.imshow(arr.reshape(28,28), cmap='gray',vmin=0, vmax=1)
+            plt.axis('off')
+            plt.title(dfR.iloc[k,1])
+    
+
 # ---------------------------- PARÂMETROS GERAIS ----------------------------
 
 threshold = 128     # limite para a transfomação grayscale -> p&b
 selNdados = False   # aplica somente sem em um sub-conjunto de dados
 N1 = 100            # número de dados de treinamento
 N2 = 10             # número de dados de teste
-N = 10              # número de experimentos
+N = 1              # número de experimentos
 
 # Parâmetros da WiSARD
 addressSize = 14     # número de bits de enderaçamento das RAMs
@@ -66,7 +101,9 @@ Y_tr = list(map(str,D_tr.index.to_list()))
 Y_te = list(map(str,D_te.index.to_list()))
 
 
+
 # --------------------------- APLICAÇÃO DA WiSARD --------------------------- 
+
 Acc = np.array([]) # matriz de acurácia
 for n in range(N):
     
@@ -107,7 +144,11 @@ Acc_std = Acc.std()
 print("\nAcurácia média: {0:1.2f} \u00B1 {1:1.2f}".format(Acc.mean(), Acc.std()))
 
 # Matriz de confusão do último experimento
-C = confusion_matrix(Y,G)
-print("\nMatriz de confusão:")
+# Linhas: Y
+# Colunas: G
+labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+C = confusion_matrix(Y,G,labels)
+print("\nMatriz de confusão do último experimento:")
 print(C)
 
+plotaExemplaresConfusao(X_te,Y,G,3,8)
